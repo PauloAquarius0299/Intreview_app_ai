7366d74091bc8790d9e421dd64e019cdd145b77e
Paulo Cesar <paulocgsantos99@gmail.com>
2025-08-07 12:53:28 -0300

    deploy initial

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Mona_Sans } from "next/font/google";
+import "./globals.css";
+
+const monaSans = Mona_Sans({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+
+export const metadata: Metadata = {
+  title: "Entrevista com AI",
+  description: "An AI-powered interview assistant for preparing for job interviews and testing your knowledge.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark">
+      <body
+        className={` ${monaSans.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
